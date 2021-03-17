63c3b32c9da2cc76ab67f3fda2aac34cb47badfa
Anvi Alex <epononanvialex@gmail.com>
2021-03-17 22:53:29 +0200

    Fix issue observed in first PR & improved the algo

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,14 +2,18 @@ def bubble_sort(array)
   is_sorted = false
   until is_sorted
     j = 0
+    count = 0
     is_sorted = true
-    while j < array.length - 1
-      if array[j] >= array[j + 1]
+    while j < array.length - 1 - count
+      temp = array[j] - array[j + 1]
+      if temp.positive?
         array[j], array[j + 1] = array[j + 1], array[j]
         is_sorted = false
+        p array
       end
       j += 1
     end
+    count += 1
   end
   array
 end
